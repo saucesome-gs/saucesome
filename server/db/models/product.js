02200baf271094de0947a8cf13a6a109f0571683
Sarah Grossman <sarah.beth.grossman@gmail.com>
2018-02-27 11:51:07 -0500

    product model added

---
+++ server/db/models/product.js
@@ -0,0 +1,40 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Product = db.define('product', {
+  brand: {
+    type: Sequelize.STRING,
+    allowEmpty: false,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowEmpty: false,
+  },
+  description: {
+    type: Sequelize.TEXT,
+  },
+  spiciness: {
+    type: Sequelize.FLOAT,
+    validate: {
+      min: 0,
+      max: 10
+    }
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+  },
+  imageUrl: {
+    type: Sequelize.TEXT,
+    validate: {
+      isUrl: true
+    }
+  },
+  tags: {
+    type: Sequelize.ARRAY(Sequelize.STRING),
+  }
+
+})
+
+module.exports = {
+  Product
+}
