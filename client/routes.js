396694bfd1c8453b54b6fcc5a865dcb1f55329a3
Sarah Grossman <sarah.beth.grossman@gmail.com>
2018-02-27 21:22:52 -0500

    connected components render

--- client/routes.js
+++ client/routes.js
@@ -2,9 +2,8 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-
 import {Login, Signup, UserHome, AllProducts, Cart} from './components'
-import {me} from './store'
+import {me, fetchProducts } from './store';
 
 
 /**
@@ -22,10 +21,11 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route exact path="/products" component={AllProducts} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/cart" component={Cart} />
-        <Route path="/products" component={AllProducts} />
+
         {
           isLoggedIn &&
             <Switch>
