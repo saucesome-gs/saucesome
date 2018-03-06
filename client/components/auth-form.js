e2c3d8e4fbc8c173f2ff6c3f0c43b67a6331b2ae
Sarah Grossman <sarah.beth.grossman@gmail.com>
2018-03-06 15:22:40 -0500

    working on adding to cart

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
-import {auth} from '../store';
+import {auth, addItemToDb} from '../store';
 
 /**
  * COMPONENT
  */
-const AuthForm = (props) => {
-  const {name, displayName, handleSubmit, error} = props
+class AuthForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit (evt) {
+    evt.preventDefault()
+    const formName = evt.target.name
+    const email = evt.target.email.value
+    const password = evt.target.password.value
+    this.props.auth(email, password, formName)
+    .then(() => console.log('cart is', this.props.cart));
+    this.addCartToDb();
+  }
+
+  addCartToDb () {
+    const itemIds = Object.keys(this.props.cart);
+    const orderId = this.props.order;
+    itemIds.map(id => {
+      // for (var i = 0; i < this.props.cart[id]; i++) {
+      //   this.props.addItemToDb(id, orderId);
+      addItemToDb(id, orderId)
+      })
+  }
+
+  render() {
+
+  const {name, displayName, error} = this.props;
+
 
   return (
     <div>
-      <form onSubmit={handleSubmit} name={name}>
+      <form onSubmit={this.handleSubmit} name={name}>
         <div>
           <label htmlFor="email"><small>Email</small></label>
           <input name="email" type="text" />
@@ -29,6 +57,7 @@ const AuthForm = (props) => {
     </div>
   )
 }
+}
 
 /**
  * CONTAINER
@@ -41,7 +70,10 @@ const mapLogin = (state) => {
   return {
     name: 'login',
     displayName: 'Login',
-    error: state.user.error
+    error: state.user.error,
+    cart: state.cart,
+    products: state.products,
+    order: state.order
   }
 }
 
@@ -49,19 +81,15 @@ const mapSignup = (state) => {
   return {
     name: 'signup',
     displayName: 'Sign Up',
-    error: state.user.error
+    error: state.user.error,
+    cart: state.cart
   }
 }
 
 const mapDispatch = (dispatch) => {
   return {
-    handleSubmit (evt) {
-      evt.preventDefault()
-      const formName = evt.target.name
-      const email = evt.target.email.value
-      const password = evt.target.password.value
-      dispatch(auth(email, password, formName));
-    }
+    auth: (email, password, formName) => dispatch(auth(email, password, formName)),
+    addItemToDb: (itemId, orderId) => dispatch(addItemToDb(itemId, orderId))
   }
 }
 
@@ -74,6 +102,7 @@ export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
 AuthForm.propTypes = {
   name: PropTypes.string.isRequired,
   displayName: PropTypes.string.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
-  error: PropTypes.object
+  // handleSubmit: PropTypes.func.isRequired,
+  error: PropTypes.object,
+  cart: PropTypes.object
 }
